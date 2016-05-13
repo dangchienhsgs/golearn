551ef8f0b76d87a32fbcfa4b5bc0e19da6d77ab4
Chien Nguyen <dangchienhsgs@gmail.com>
2016-05-13 18:33:22 +0700

    Add some abstract function to LDAModel

---
+++ lda/lda.go
@@ -0,0 +1,22 @@
+package lda
+
+import (
+	"github.com/sjwhitworth/golearn/base"
+	"math"
+)
+
+type LDAModel struct {
+}
+
+// New instance of LDAModel
+func NewLDAModel() *LDAModel {
+
+}
+
+func (model *LDAModel) Fit(X base.FixedDataGrid) {
+
+}
+
+func (model *LDAModel) Infer() base.FixedDataGrid {
+
+}
